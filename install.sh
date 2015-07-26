f78283c90c956bcd900eef36826bca9d5d4c36d3
jshou <joshuahou@gmail.com>
2015-07-26 14:33:35 -0700

    finish translating install script

--- install.sh
+++ install.sh
@@ -6,19 +6,52 @@ backup_all=false
 
 for linkable in `ls */**.symlink`
 do
-    overwrite=false
-    backup=false
+  overwrite=false
+  backup=false
 
-    file=`basename $linkable | sed s/.symlink//`
-    target="$HOME/.$file"
+  file=`basename $linkable | sed s/.symlink//`
+  target="$HOME/.$file"
 
-    if [ -e $target ] | [ -L $target ]
+  if [ -e $target ] | [ -L $target ]
+  then
+    if [ skip_all != true ] || [ overwrite_all != true ] || [ backup_all != true ]
     then
-      if [ skip_all != true ] || [ overwrite_all != true ] || [ backup_all != true ]
-      then
-        printf "File already exists: $target, what do you want to do? [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all "
-        read input
-        echo "your input was: $input"
-      fi
+      printf "File already exists: $target, what do you want to do? [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all "
+      read input
+      echo "your input was: $input"
+
+      case $input in
+        o )
+          overwrite=true
+          ;;
+        b )
+          backup=true
+          ;;
+        O )
+          overwrite_all=true
+          ;;
+        B )
+          backup_all=true
+          ;;
+        S )
+          skip_all=true
+          ;;
+        s )
+          continue
+          ;;
+      esac
     fi
+
+    if [ overwrite == true ] || [ overwrite_all == true ]
+    then
+      rm $target
+    fi
+
+    if [ backup == true ] || [ backup_all == true ]
+    then
+      mv "$HOME/.$file" "$HOME/.$file.backup"
+    fi
+  fi
+
+  ln -s "$PWD/$linkable" "$target"
 done
