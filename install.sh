128e671706824613d9208cc5d5d2ddf41ed001f0
jshou <joshuahou@gmail.com>
2015-07-26 10:35:33 -0700

    WIP more progress?

--- install.sh
+++ install.sh
@@ -11,5 +11,12 @@ do
 
     file=`basename $linkable | sed s/.symlink//`
     target="$HOME/.$file"
-    echo $target
+
+    if [ -e $target ] | [ -L $target ]
+    then
+      if [ skip_all != true ] || [ overwrite_all != true ] || [ backup_all != true ]
+      then
+        echo "File already exists: $target, what do you want to do? [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all"
+      fi
+    fi
 done
