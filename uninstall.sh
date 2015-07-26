eb349ce868a76de1b5b640c4512eb7ef8c9644d2
jshou <joshuahou@gmail.com>
2015-07-26 14:36:45 -0700

    converted uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for linkable in `ls */**.symlink`
+do
+  file=`basename $linkable | sed s/.symlink//`
+  target="$HOME/.$file"
+
+  # remove all symlinks created during installation
+  if [ -L $target ]
+  then
+    rm $target
+  fi
+
+  # replace any backups made during installation
+  if [ -e "$HOME/.$file.backup" ]
+    mv "$HOME/.$file.backup" "$HOME/.$file"
+  fi
+done
