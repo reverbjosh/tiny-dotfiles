dbfcc0d8a6696efa97711360fc92e7990d1aca3b
jshou <joshuahou@gmail.com>
2017-02-15 11:52:37 -0800

    document this block

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ skip_all=false
 overwrite_all=false
 backup_all=false
 
+# install base16 colors
 if [ ! -d ~/.config/base16-shell ]; then
   git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 fi
